6a04afd7549c27abe06bc64954aec5d2c71532c7
codecookride <57050292+codecookride@users.noreply.github.com>
2020-08-28 19:05:53 -0700

    Updates

---
+++ precache-manifest.f71cc703d1cdeeb3df2b2106bc14ff6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d7a95e149826955fe770ad8bae2e83",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "3c9d47e8174ac2422f05",
+    "url": "/game/static/css/main.ee036672.chunk.css"
+  },
+  {
+    "revision": "04723106dafa1fefc1ec",
+    "url": "/game/static/js/2.f179b4b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game/static/js/2.f179b4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9d47e8174ac2422f05",
+    "url": "/game/static/js/main.d9e8fa68.chunk.js"
+  },
+  {
+    "revision": "535c13a4cd88f6116753",
+    "url": "/game/static/js/runtime-main.a292d6c1.js"
+  }
+]);
